3ab60d4c04aaee1460e862ac08d9c89d3a0a3bd2
suddenflamingo1 <113945929+suddenflamingo1@users.noreply.github.com>
2023-10-28 00:22:14 -0500

    Add files via upload

---
+++ xubuntu-bloatware-remove-tool.sh
@@ -0,0 +1,21 @@
+sudo apt remove parole
+sudo apt remove rhythmbox
+sudo apt remove xfburn
+sudo apt remove hexchat
+sudo apt remove libreoffice-draw
+sudo apt remove libreoffice-calc
+sudo apt remove transmission-gtk
+snap remove chromium
+sudo apt-get remove --purge libreoffice*
+sudo apt-get clean
+sudo apt-get autoremove
+sudo apt remove sgt-launcher
+sudo apt remove gnome-mines
+sudo apt remove gnome-sudoku
+sudo apt remove gigolo
+sudo apt-get autoremove
+sudo apt-get remove chromium-browser
+sudo apt-get remove --auto-remove chromium-browser
+sudo apt-get purge chromium-browser
+sudo apt-get purge --auto-remove chromium-browser
+sudo apt-get autoremove
